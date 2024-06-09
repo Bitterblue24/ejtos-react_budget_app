d8858935a75b4528a95fd32e8a4c73ec2661bdaf
Bitterblue24 <taylorguasta24@gmail.com>
2024-06-09 10:36:21 -0400

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,22 @@
-
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
+const error = () => {
+if (newBudget.value > 20000);
+    alert("Budget cannot exceed 20,000 euros");
+if (newBudget.value < TotalExpenses.value);
+    alert("Budget cannot be lower than ammount already spent");
+else (error = false)
+}
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
+        event.preventDefault();
+        const error = validateFormData(newBudget.value)
         setNewBudget(event.target.value);
     }
+
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
